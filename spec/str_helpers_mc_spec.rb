8896c0c85c578574e410339f64f1daba972a8859
Micah Caffey <mrcaffey@gmail.com>
2018-10-15 10:54:35 -0600

    edited str_helpers_mc.spec

--- spec/str_helpers_mc_spec.rb
+++ spec/str_helpers_mc_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe StrHelpersMc do
+  subject { StrHelperMc }
   it "has a version number" do
     expect(StrHelpersMc::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do
+
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string'
+    expect(subject.reversify(str)).to eq('gnirtS yM')
   end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y  S t r i n g')
+  end
+
+
 end
